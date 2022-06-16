4dbf861123e488a1aaea5f6d4bc2beee341448fc
PriyaRNair27 <priyarnair2710@gmail.com>
2022-06-16 13:06:43 +0530

    aethanalystica

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div class="row g-3">
+                    <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                      
+                        <nav class="navbar navbar-expand-lg bg-white">
+                            <div class="container-fluid">
+                              <a class="navbar-brand text-dark" href="#">Social<label class="text-info ">Cash</label></a>
+                            </div>
+                            </nav>
+                              
+                              </div>
+                              <div style="text-align:center">
+                                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                    <label for="" class="form-label"> <h3>A REVOLUTION IN SOCAIL MEDIA</h3></label>
+                                    </div>
+                                    <div style="text-align:center">
+                                        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                            <label for="" class="form-label"> <h3>MARKETING IS COMING <br><br></h3></label>
+                                            </div>
+
+                                    <div style="text-align:center">
+                                        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                            <label for="" class="form-label"> <h3>BE A PART OF IT <br><br></h3></label>
+                                            </div>
+                            </div>
+                            <div style="text-align:center">
+                                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                    <label for="" class="text-muted" class="form-label " >Join our Telegram channel to get Early bird advantages<br><br> </label>
+                                    </div>
+                                    <div style="text-align:center">
+                                        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                            <button   style=" width:300px"  class=" btn btn-outline-primary">
+                                                
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"  class="bi bi-telegram" viewBox="0 0 16 16">
+                                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"/>
+                                              </svg>
+                                              <strong>JOIN US</strong></button>
+                                        </div> 
+                            </div>
+                            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <img src="Assignment.png" width="750" height="400" >
+                                 </div>
+
+                </div>
+            </div>
+        </div>
+    
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+</body>
+</html>
